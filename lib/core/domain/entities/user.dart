bade31ed60cdb034bdfe43857b3d72f04e069d09
Sygix <17769298+Sygix@users.noreply.github.com>
2024-04-24 19:16:48 +0200

    !feat(auth): create user entity + auth api

---
+++ lib/core/domain/entities/user.dart
@@ -0,0 +1,35 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as models;
+import 'package:flutter/foundation.dart';
+import 'package:onmangeou/shared/utils.dart';
+
+class User extends ChangeNotifier{
+  late models.User _user;
+  late String _username;
+  late String _id;
+  
+  //Getters
+  String get username => _username;
+  String get id => _id;
+
+  //Constructor
+  User({
+    required models.User user
+  }){
+    _user = user;
+    initUser(user: user);
+  }
+
+  //Methods
+  void initUser({ required models.User user }) {
+    try{
+      _id = user.$id;
+      _username = user.name;
+    } on AppwriteException catch(e) {
+      Utils.logError(message: 'Init user failed', error: e);
+    } finally {
+      notifyListeners();
+    }
+  }
+
+}
---
+++ lib/shared/utils.dart
@@ -0,0 +1,11 @@
+import 'dart:developer';
+
+class Utils {
+  static logDebug({required dynamic message, Object? error}) {
+    log(message.toString(), time: DateTime.now(), name: 'DEBUG', level: 700, error: error);
+  }
+
+  static logError({required dynamic message, Object? error}) {
+    log(message.toString(), time: DateTime.now(), name: 'ERROR', level: 1000, error: error);
+  }
+}
