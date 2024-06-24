fec365e28130f2fc5f5d480b11668bf69c2c728a
Sygix <17769298+Sygix@users.noreply.github.com>
2024-06-24 19:51:11 +0200

    fix: change app constant cache duration

--- lib/shared/constants/app.dart
+++ lib/shared/constants/app.dart
@@ -1,4 +1,4 @@
 class AppConstants {
   static const int resendEmailWaitTime = 30;
-  static const Duration cacheExpirationTime = Duration(hours: 24);
+  static const Duration cacheExpirationTime = Duration(minutes: 1); // 1 week
 }
