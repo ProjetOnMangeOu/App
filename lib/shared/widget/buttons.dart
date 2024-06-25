ec5a244164c868bb81726d35ab889c7877a45549
Pire <p.clement01@orange.fr>
2024-06-25 15:07:06 +0200

    style(general) Buttons

---
+++ lib/shared/widget/buttons.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class OMGUButtons extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final Color color;
+
+  const OMGUButtons({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.black,
+          backgroundColor: this.color, // Text color
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 15.0),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
